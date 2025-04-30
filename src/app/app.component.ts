39fc658caf37ea8062d92f9002c7ae4cdac7265c
Davi Chechetto Westphal <davi.48515@alunosatc.edu.br>
2025-04-29 21:47:05 -0300

    Fazendo Carrossel com Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-project';
+
+  contador = signal(0)
+
+  imagemContador = signal(1)
+
+  imagemList = signal([
+    'imagem1.png',
+    'imagem2.png',
+    'imagem3.png'
+  ])
+
+  decrementar(){
+    let currentValue = this.contador()
+    this.contador.set(--currentValue)
+  }
+  incrementar(){
+    let currentValue = this.contador()
+    this.contador.set(++currentValue)
+  }
+
+  proximo(){
+    console.log(this.imagemList.length);
+    
+    if(this.imagemContador() >= this.imagemList.length){
+      return
+    } else{
+      let currentValue = this.imagemContador()
+      this.imagemContador.set(++currentValue)
+    }
+  }
+  anterior(){
+    if(this.imagemContador() < 1){
+      return
+    } else{
+      let currentValue = this.imagemContador()
+      this.imagemContador.set(--currentValue)
+    }
+  }
 }
